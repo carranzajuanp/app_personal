9a3c85d8a9ef4ac7abed13f5741fc774355c1dd2
carranzajuanp <carranzajuanp@gmail.com>
2025-03-19 20:03:09 -0300

    beta

---
+++ app.R
@@ -0,0 +1,57 @@
+library(shiny)
+
+ui <- fluidPage( 
+  sidebarLayout(
+    sidebarPanel(
+      uiOutput('choose_course')
+    ),
+    mainPanel(
+      tableOutput('courseTable')
+    )
+  )
+)
+
+server <- function(input, output, session) {
+  nombres = c("Juan", "Pedro", "José", "María", "Susana", "Clara")
+  apellidos = c("Aaaaa", "Bbbbb", "Cccccc", "Dddddd", "Eeeeee", "Ffffff", "Gggggg")
+  paste0(sample(nombres, 1), " ", sample(apellidos, 1))
+  cargo = c("Docente", "Nodocente")
+  opciones =  c("Facultad de Ciencias Médicas",
+                "Facultad de Ciencias Exactas, Físicas y Naturales",
+                "Escuela Superior de Comercio Manuel Belgrano",
+                "Facultad de Derecho",
+                "Facultad de Arquitectura, Urbanismo y Diseño",
+                "Facultad de Ciencias Económicas",
+                "Facultad de Ciencias Químicas",
+                "Facultad de Filosofía Y Humanidades",
+                "Facultad de Odontología",
+                "Área Central",
+                "Colegio Nacional de Monserrat",
+                "Facultad de Ciencias Agropecuarias",
+                "Facultad de Lenguas",
+                "Facultad de Psicología",
+                "Facultad de Artes",
+                "Facultad de Ciencias de la Comunicación",
+                "Facultad de Matemática, Astronomía, Física y Computación",
+                "Facultad de Ciencias Sociales",
+                "Observatorio Astronómico")
+  n = 50
+  datos = reactive({ data.frame(Unidad = replicate(n, sample(opciones, 1)),
+                     Nombre = replicate(n, paste0(sample(nombres, 1), " ", sample(apellidos, 1))),
+                     Cargo = replicate(n, sample(cargo, 1))
+  )
+  })
+  
+  output$choose_course <- renderUI({
+    course.names <- as.vector( unique(datos()$Unidad) )
+    selectInput("facultad", "Seleccionar:", choices=course.names, multiple=TRUE)    
+  })
+  
+
+  model.data <- reactive({
+    subset(datos(), Unidad %in% input$facultad)
+  })
+  
+  output$courseTable <- renderTable({ model.data() })
+}
+runApp(shinyApp(ui,server))
